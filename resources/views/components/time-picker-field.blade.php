85f1127ff47f494e9457dc6195f995d878ddf281
HUSAM TAREQ AL-SHAIBANI <hu22am1@gmail.com>
2025-09-10 07:20:37 +0300

    Update time-picker-field.blade.php
    fix validation error

--- resources/views/components/time-picker-field.blade.php
+++ resources/views/components/time-picker-field.blade.php
@@ -24,7 +24,7 @@
         :suffix-actions="$suffixActions"
         :suffix-icon="$suffixIcon"
         :suffix-icon-color="$getSuffixIconColor()"
-        :valid="! $errors->has($statePath)"
+        :valid="! $errors->has($getStatePath())"
         :attributes="\Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())"
     >
 
