1a94dc93c71fd4432d4de134317371c5076c2aba
HUSAM TAREQ AL-SHAIBANI <hu22am1@gmail.com>
2025-09-23 15:41:45 +0300

    Add statePath variable for time picker input
    fix Undefined variable $_instance error in Model form

--- resources/views/components/time-picker-field.blade.php
+++ resources/views/components/time-picker-field.blade.php
@@ -1,6 +1,7 @@
 @php
     $fieldWrapperView = $getFieldWrapperView();
     $id = $getId();
+    $statePath = $getStatePath();
     $isDisabled = $isDisabled();
     $extraInputAttributeBag = $getExtraInputAttributeBag();
     $extraAlpineAttributes = $getExtraAlpineAttributes();
@@ -18,58 +19,40 @@
     $isRequired = $isRequired();
 
 @endphp
-<x-dynamic-component
-    :component="$fieldWrapperView"
-    :field="$field"
-    :inline-label-vertical-alignment="\Filament\Support\Enums\VerticalAlignment::Center"
->
+<x-dynamic-component :component="$fieldWrapperView" :field="$field" :inline-label-vertical-alignment="\Filament\Support\Enums\VerticalAlignment::Center">
 
-    <x-filament::input.wrapper
-        :disabled="$isDisabled"
-        :inline-prefix="$isPrefixInline"
-        :inline-suffix="$isSuffixInline"
-        :prefix="$prefixLabel"
-        :prefix-actions="$prefixActions"
-        :prefix-icon="$prefixIcon"
-        :prefix-icon-color="$getPrefixIconColor()"
-        :suffix="$suffixLabel"
-        :suffix-actions="$suffixActions"
-        :suffix-icon="$suffixIcon"
-        :suffix-icon-color="$getSuffixIconColor()"
-        :valid="! $errors->has($getStatePath())"
-        :attributes="\Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())"
-    >
+    <x-filament::input.wrapper :disabled="$isDisabled" :inline-prefix="$isPrefixInline" :inline-suffix="$isSuffixInline" :prefix="$prefixLabel" :prefix-actions="$prefixActions"
+        :prefix-icon="$prefixIcon" :prefix-icon-color="$getPrefixIconColor()" :suffix="$suffixLabel" :suffix-actions="$suffixActions" :suffix-icon="$suffixIcon" :suffix-icon-color="$getSuffixIconColor()"
+        :valid="!$errors->has($statePath)" :attributes="\Filament\Support\prepare_inherited_attributes($getExtraAttributeBag())">
 
 
-        <input   {{
-                    $extraInputAttributeBag
-                        ->merge($extraAlpineAttributes, escape: false)
-                        ->merge([
-                            'autofocus' => $isAutofocused,
-                            'disabled' => $isDisabled,
-                            'placeholder' => $placeholder,
-                            'readonly' => $isReadOnly,
-                            'required' => $isRequired && (! $isConcealed),
-                        ], escape: false)
-                        }}  type="time" x-ref="timePicker" x-data="mdtimepicker($refs.timePicker, {
-                okLabel: '{{ $getOkLabel() }}',
-                cancelLabel: '{{ $getCancelLabel() }}',
-                format: 'hh:mm:ss',
-                timeFormat: 'hh:mm:ss',
-                events: {
-                    timeChanged: function(data, timepicker) {
-                        @this.set('{!! $getStatePath() !!}', data.time);
-                    },
-
-                }
-            },
-
-        )"
-
-            {{ $applyStateBindingModifiers('wire:model') }}="{{ $getStatePath() }}" @class([
+        <input
+            {{ $extraInputAttributeBag->merge($extraAlpineAttributes, escape: false)->merge(
+                [
+                    'autofocus' => $isAutofocused,
+                    'disabled' => $isDisabled,
+                    'placeholder' => $placeholder,
+                    'readonly' => $isReadOnly,
+                    'required' => $isRequired && !$isConcealed,
+                ],
+                escape: false,
+            ) }}
+            type="time" x-ref="timePicker" x-data="mdtimepicker($refs.timePicker, {
+                    okLabel: '{{ $getOkLabel() }}',
+                    cancelLabel: '{{ $getCancelLabel() }}',
+                    format: 'hh:mm:ss',
+                    timeFormat: 'hh:mm:ss',
+                    events: {
+                        timeChanged: function(data, timepicker) {
+                            $wire.set('{!! $statePath !!}', data.time);
+                        },
+            
+                    }
+                },
+            
+            )"
+            {{ $applyStateBindingModifiers('wire:model') }}="{{ $statePath }}" @class([
                 'time-input-picker fi-input block w-full border-none bg-transparent text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] sm:text-sm sm:leading-6',
-
             ])>
-        </x-filament::input.wrapper>
+    </x-filament::input.wrapper>
 </x-dynamic-component>
-
